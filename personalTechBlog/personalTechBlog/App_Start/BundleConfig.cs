b67b55035ddb6d37395c8eb1ab7fdb6f1d58c2f4
thiago-nsjob <thiago.nsjob@gmail.com>
2017-10-27 14:30:59 -0200

    bundle names was conflicting with folder names.

--- personalTechBlog/personalTechBlog/App_Start/BundleConfig.cs
+++ personalTechBlog/personalTechBlog/App_Start/BundleConfig.cs
@@ -22,13 +22,15 @@ namespace personalTechBlog
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js",
                       "~/Scripts/respond.js"));
-
+              
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
 
-            bundles.Add(new StyleBundle("~/Content/blog").Include(
+            bundles.Add(new StyleBundle("~/Content/bundleBlog").Include(
                      "~/Content/blog/base.css"));
+
+            BundleTable.EnableOptimizations = true;
         }
     }
 }
